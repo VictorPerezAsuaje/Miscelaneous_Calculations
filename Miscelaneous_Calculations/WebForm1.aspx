2f314c5ff3a04bb631e8a2c24a116999b0a27309
Victor Pérez <v.perezasuaje@gmail.com>
2020-05-15 20:41:22 +0200

    Add project files.

---
+++ Miscelaneous_Calculations/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Miscelaneous_Calculations.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Miscelaneous Calculations</h1>
+            <h3>Fibonacci sequence</h3>
+            <asp:Label ID="FiboNumbers" runat="server"></asp:Label>
+            <br />
+            <p>Elige el número de cifras a mostrar (0 - 50):</p>
+            <asp:TextBox ID="CifrasFibo" runat="server" Width="73px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BtnCalcFibo" runat="server" OnClick="BtnCalcFibo_Click" Text="Calculate" />
+            <br />
+            <h3>Is it Fibonacci?</h3>
+            <asp:TextBox ID="TxtNumberIsFibo" runat="server" Width="69px"></asp:TextBox>
+&nbsp;
+            <asp:Label ID="AnswerIsFibo" runat="server"></asp:Label>
+            <br />
+            <br />
+
+            <asp:Button ID="BtnIsFibo" runat="server" OnClick="BtnIsFibo_Click" Text="Is it?" />
+
+            <br />
+            <h3>Find the N number of Pi</h3>
+            <asp:Label ID="PiLbl" runat="server" Text="3,1415..."></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="TxtBoxAllResults" runat="server" Height="81px" ReadOnly="True" TextMode="MultiLine" Visible="False" Width="861px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="TxtNPi" runat="server" Width="68px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="BtnNOfPi" runat="server" OnClick="BtnNOfPi_Click" Text="Search" />
+&nbsp;&nbsp;
+            <asp:Button ID="BtnNextIndexPi" runat="server" OnClick="BtnNextIndexPi_Click" Text="All the places where it appears" Visible="False" />
+&nbsp;&nbsp;
+            <asp:Button ID="BtnClear" runat="server" OnClick="BtnClear_Click" Text="Clear TextBox" />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Miscelaneous_Calculations/WebForm1.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Miscelaneous_Calculations
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnCalcFibo_Click(object sender, EventArgs e)
+        {
+            FiboNumbers.Text = String.Empty;
+            long[] Fibonacci = new long[50];
+            Fibonacci[0] = 0;
+            Fibonacci[1] = 1;
+            int i;
+
+            for (i = 2; i < Fibonacci.Length; i++)
+            {
+                Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
+            }
+
+            i = 0;
+            if(Convert.ToInt32(CifrasFibo.Text)> 50)
+            {
+                MessageBox.Show("Número menor a 50","Número inválido", MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                while (i < Convert.ToInt32(CifrasFibo.Text))
+                {
+                    FiboNumbers.Text += Fibonacci[i];
+                    FiboNumbers.Text += " ";
+                    i++;
+                }
+            }
+            
+        }
+        public bool IsFibonacci(int numero)
+        {
+            int [] Fibonacci = new int[50];
+            Fibonacci[0] = 0;
+            Fibonacci[1] = 1;
+            int i;
+            bool respuesta = false;
+
+            for (i = 2; i < Fibonacci.Length; i++)
+            {
+                Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i - 2];
+                if (Fibonacci[i] == numero)
+                {
+                    respuesta = true;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return respuesta;
+        }
+
+        protected void BtnIsFibo_Click(object sender, EventArgs e)
+        {
+            if(IsFibonacci(Convert.ToInt32(TxtNumberIsFibo.Text)) == true)
+            {
+                AnswerIsFibo.Text = "Yes it is!";
+            }
+            else
+            {
+                AnswerIsFibo.Text = "No, it is not...";
+            }
+                
+        }
+
+        protected void BtnNOfPi_Click(object sender, EventArgs e)
+        {
+            const double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
+            int localizacion = pi.ToString().IndexOf(TxtNPi.Text);
+            PiLbl.Text = "The number " + TxtNPi.Text + " is first located in the position " + localizacion + " of Pi.";
+            BtnNextIndexPi.Visible = true;
+        }
+
+        protected void BtnNextIndexPi_Click(object sender, EventArgs e)
+        {
+            string pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+            TxtBoxAllResults.Visible = true;
+            char[] numeroPi = pi.ToCharArray();
+            int i = 0;
+            //PiLbl.Text = String.Empty;
+
+            for(i = 0; i < numeroPi.Length; i++)
+            {
+                //PiLbl.Text += numeroPi[i];
+
+                if (numeroPi[i] == Convert.ToChar(TxtNPi.Text))
+                {
+                    TxtBoxAllResults.Text += "The number " + TxtNPi.Text + " is next located in the position " + i + " of Pi." + Environment.NewLine;
+                }
+                else
+                {
+                    continue;
+                }
+            }            
+                
+        }
+
+        protected void BtnClear_Click(object sender, EventArgs e)
+        {
+            TxtBoxAllResults.Text = String.Empty;
+        }
+    }
+}
